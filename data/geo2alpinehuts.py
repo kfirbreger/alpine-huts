8f60e93b28306f6efe37cca608ff5cde41349adf
Kfir Breger <kfir@conopa.co>
2021-03-29 16:32:30 +0200

    Working on standardizing the data

--- data/geo2alpinehuts.py
+++ data/geo2alpinehuts.py
@@ -3,20 +3,44 @@ Converts geojson data into alpine huts json.
 """
 import json
 
+new_props = []
+
+
+def chain_get(prop, chain):
+    """
+    Recustivly go through the chain, returning the first value
+    encountered
+    """
+    data = prop.get(chain[0], None)
+    if not data:
+        if len(chain) > 1:
+            data = chain_get(prop, chain [1:])
+    return data
 
 def convert_feature(feature):
     data = {}
+    main_properties = []
     properties = feature.get('properties', None)
     if properties:
-        # Basic properties
+        data['email'] = chain_get(properties, ['contanct:email', 'email'])
+        data['website'] = chain_get(properties, ['contact:website', 'website', 'website2', 'operator:website'])
+        data['name'] = chain_get(properties, ['name', 'reg_name', 'official_name', 'short_name'])
+        """
         data['name'] = properties.get('name', None)
-        data['website'] = properties.get('website', None)
-        data['tel'] = properties.get('phone', None)
+        # Checking for name in different languages
+        """
+        for prop in main_properties:
+            data[prop] = properties.get(prop, None)
         # Extended properties
-        data['extended'] = {
+        #data['extended'] = {
+        for k in properties.keys():
+            if k not in main_properties and k not in new_props:
+                print(k)
+                new_props.append(k)
 
     # Adding the geolocation. For now derived from middle
     data['coordinates'] = feature['geometry']['coordinates']
+    return data
 
 
 def convert_file(file_name):
