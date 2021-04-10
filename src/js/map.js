f44810f440ad2afbba7c40099b4887d6a24d4f87
Kfir Breger <kfir@conopa.co>
2021-04-11 00:06:11 +0200

    Clustering huts. Now to display counts in the cluster

--- src/js/map.js
+++ src/js/map.js
@@ -8,7 +8,70 @@ function createMap() {
   });
   // Add zoom and rotation controls to the map.
   map.addControl(new mapboxgl.NavigationControl());
+
+  // @TODO look into this https://docs.mapbox.com/mapbox-gl-js/example/hillshade/
+
+  map.on('load', loadHutsGeojson);
   return map;
+}
+
+function loadHutsGeojson() {
+  const map = this;  // Clarifies what we are actually working on
+  map.addSource('frHuts', {
+    type: 'geojson',
+    data: 'data/fr.geojson',
+    cluster: true,
+    clusterRadius: 50
+  });
+
+  map.addLayer({
+    id: 'fr_markers',
+    type: 'circle',
+    source: 'frHuts',
+    filter: ['!=', 'cluster', true]
+  });
+  // Objects to keep track of markers in memory and visible
+  const markers = {};
+  let visibleMarkers = {};
+  function updateMarkers() {
+    const newMarkers = {};
+    const features = map.querySourceFeatures('frHuts');
+    // Create html for each cluster marker with no marker
+    for (let i = 0; i < features.length; i++) {
+      const coords = features[i].geometry.coordinates;
+      let props = features[i].properties;
+      // If not in cluster, move on
+      if (!props.cluster) {
+        continue;
+      }
+      const id = props.cluster_id;
+      let marker = markers[id];
+      if (!marker) {
+        marker = markers[id] = new mapboxgl.Marker().setLngLat(coords);
+      }
+      newMarkers[id] = marker;
+
+      if (!visibleMarkers[id]) {
+        marker.addTo(map);
+      }
+    }
+    // Removing all no longer visible cluster markers
+    for (id in visibleMarkers) {
+      if (!newMarkers[id]) {
+        visibleMarkers[id].remove();
+      }
+    }
+
+    visibleMarkers = newMarkers;
+  }
+  // Updating visible markers on each map render
+  map.on('render', function() {
+    if (!map.isSourceLoaded('frHuts')) {
+      return;
+    }
+    updateMarkers();
+  });
+      
 }
 
 function loadHuts(url) {
@@ -36,10 +99,10 @@ function addHutToMap(hut, map) {
 
 
 function getAndDisplayHuts(map) {
-  const dataset = ['fr.json'];
+  const dataset = ['fr.geojson'];
   for (let i = 0; i < dataset.length; i++) {
     loadHuts('data/' + dataset[i])
-    .then(huts => {
+   .then(huts => {
       for (let i = 0; i < huts.length;i++) {
         addHutToMap(huts[i], map);
       }
@@ -50,5 +113,5 @@ function getAndDisplayHuts(map) {
 
 document.addEventListener("DOMContentLoaded", () => {
   const map = createMap();
-  getAndDisplayHuts(map);
+  //getAndDisplayHuts(map);
 });
